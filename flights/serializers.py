cfc12fac5a53ae92e96f4f62c0d74724b439538b
Waleedx7 <w.alshelahi@gmail.com>
2023-02-02 20:30:06 +0300

    done

--- flights/serializers.py
+++ flights/serializers.py
@@ -33,11 +33,11 @@ class UserCreateSerializer(serializers.ModelSerializer):
         model = User
         fields = ["username", "password","first_name","last_name"]
     def create(self, validated_data):
-        username = validated_data["username"]
+
         password = validated_data.pop("password")
-        first_name = validated_data["first_name"]
-        last_name = validated_data["last_name"]
-        new_user = User(username=username,first_name=first_name,last_name=last_name)
+        
+        
+        new_user = User(**validated_data)
         new_user.set_password(password)
         new_user.save()
         return validated_data
